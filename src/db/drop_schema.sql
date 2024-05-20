ed8237dd4cbecee484a0bfdd989cfbacd758d695
José Maykol <jpaniura@unsa.edu.pe>
2024-05-19 19:56:54 -0500

    feat: add initial database schema and migration files

---
+++ src/db/drop_schema.sql
@@ -0,0 +1,20 @@
+-- Drop the user_read_chapters table
+DROP TABLE IF EXISTS user_read_chapters;
+
+-- Drop the user_mangas table
+DROP TABLE IF EXISTS user_mangas;
+
+-- Drop the chapters table
+DROP TABLE IF EXISTS chapters;
+
+-- Drop the manga_genres table
+DROP TABLE IF EXISTS manga_genres;
+
+-- Drop the mangas table
+DROP TABLE IF EXISTS mangas;
+
+-- Drop the genres table
+DROP TABLE IF EXISTS genres;
+
+-- Drop the users table
+DROP TABLE IF EXISTS users;
